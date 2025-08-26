c4cb0e88be5b09a3f15d5a32c4ff12616928889a
Castruche <aurelien.blot@gmail.com>
2025-08-26 09:36:01 -0400

    test cd/ci 4

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Let's start React and CD/CI</h1>
-        <h2>Auto</h2>
+      <h1>Let's start ! React and CD/CI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
