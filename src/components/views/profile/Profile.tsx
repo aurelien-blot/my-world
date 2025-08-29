c51534b23393ea832533098ba5bb1c6c3650c974
Castruche <aurelien.blot@gmail.com>
2025-08-28 22:35:56 -0400

    correction

--- src/components/views/profile/Profile.tsx
+++ src/components/views/profile/Profile.tsx
@@ -1,9 +1,6 @@
-import NavigationBar from "../../navigationBar/NavigationBar.tsx";
-
 function Profile() {
     return (
         <>
-            <NavigationBar activePage={"profile"}/>
         </>
     )
 }
--- src/components/views/settings/Settings.tsx
+++ src/components/views/settings/Settings.tsx
@@ -1,9 +1,6 @@
-import NavigationBar from "../../navigationBar/NavigationBar.tsx";
-
 function Settings() {
     return (
         <>
-            <NavigationBar activePage={"settings"}/>
         </>
     )
 }
