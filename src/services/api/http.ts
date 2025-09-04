00f595c9f845de37d4c2513a30b612a9607b23f6
Castruche <aurelien.blot@gmail.com>
2025-09-04 09:21:20 -0400

    Premier appel login fonctionnel

--- src/services/api/http.ts
+++ src/services/api/http.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_APP_API_URL + "/api/my-world",
+    baseURL: import.meta.env.VITE_APP_API_URL + "/my-world",
     headers: {
         "Content-Type": "application/json",
     },
